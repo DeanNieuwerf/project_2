398665f410ff02c3bca255bf940899fc3070368b
DeCheeBV <110696176+DeCheeBV@users.noreply.github.com>
2023-08-13 13:58:06 +0200

    'test'

--- src/project_2/utils/common.py
+++ src/project_2/utils/common.py
@@ -27,7 +27,7 @@ def read_yaml(path_to_yaml: Path) -> ConfigBox:
         with open(path_to_yaml) as yaml_file:
             content = yaml.safe_load(yaml_file)
             logger.info(f"yaml file: {path_to_yaml} loaded succesfully")
-            returns ConfigBox(content)
+            return ConfigBox(content)
     except BoxValueError:
         raise ValueError("yaml file is empty")
     except Exception as e:
